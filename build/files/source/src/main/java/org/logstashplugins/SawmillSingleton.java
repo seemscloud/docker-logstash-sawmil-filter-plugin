29c33eaf38761957adaac6ae8048164210fbbf66
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-04-17 14:57:43 +0200

    sadf

--- build/files/source/src/main/java/org/logstashplugins/SawmillSingleton.java
+++ build/files/source/src/main/java/org/logstashplugins/SawmillSingleton.java
@@ -3,16 +3,20 @@ package org.logstashplugins;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public final class SawmillSingleton {
     private static SawmillSingleton INSTANCE;
 
     private String pipeline = "";
+    private String pipelineHash = "";
 
-    private SawmillSingleton() {
-    }
+    private SawmillSingleton() { }
 
     public static SawmillSingleton getInstance() {
         if(INSTANCE == null) {
@@ -24,10 +28,13 @@ public final class SawmillSingleton {
 
     public String getPipeline(String dir, String filename) {
         try {
-            if (this.pipeline == "") {
-                File file = new File(dir + "/" + filename);
-                this.pipeline = FileUtils.readFileToString(file, "UTF-8");
-                System.out.print("Pipeline created..");
+            String path = dir + "/" + filename;
+            String hex = checksum(path, MessageDigest.getInstance("MD5"));
+
+            if (this.pipelineHash != hex) {
+                this.pipelineHash = hex;
+                this.pipeline = FileUtils.readFileToString(new File(path), "UTF-8");
+                System.out.print("Detected changes, pipeline reload..");
             } else {
                 System.out.print("Pipeline read from cache..");
             }
@@ -37,5 +44,22 @@ public final class SawmillSingleton {
         }
 
         return this.pipeline;
+    }
+
+    private static String checksum(String filepath, MessageDigest md) throws IOException {
+
+        try (InputStream fis = new FileInputStream(filepath)) {
+            byte[] buffer = new byte[1024];
+            int nread;
+            while ((nread = fis.read(buffer)) != -1) {
+                md.update(buffer, 0, nread);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
     }
 }
